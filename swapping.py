6240aa15362b32e310136f6f6eb0beb24fe4b179
sonam <kumarisonam08685@gmail.com>
2026-08-19 14:49:49 +0530

    basic code

---
+++ swapping.py
@@ -0,0 +1,8 @@
+#swapping the value of each other.
+a = 10;
+b = 30;
+
+a,b = b, a
+print("The value of a after swapping: ", a)
+
+print("The value of b after swapping: ", b)
---
+++ table.py
@@ -0,0 +1,4 @@
+#print the table of i
+a=int(input("Enter the number: "))
+for i in range(1,11):
+    print(a,"*",i,"=",a*i)
